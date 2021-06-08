47c239a32bfbbb2e384ed2758743d2c87eda6f13
Rameez-Tamboli <tambolirameez123@gmail.com>
2021-06-08 17:39:32 +0530

    Update Lab3Ex3.java

--- src/practice/Lab3Ex3.java
+++ src/practice/Lab3Ex3.java
@@ -9,7 +9,7 @@ public class Lab3Ex3 {
 			String initial, rev = "";
 			@SuppressWarnings("resource")
 			Scanner s = new Scanner(System.in);
-			System.out.println("Enter the string to reverse");
+			System.out.println("E to reverse");
 			initial = s.nextLine();
 			int length = initial.length();
 			for (int i = length - 1; i >= 0; i--)
